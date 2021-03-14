786eb91541b47fbe50454ca89260f4f9f55bc1d7
charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>
2021-03-13 17:49:44 -0700

    feat: add updates
    Signed-off-by: charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>

--- covid-vaccine-bot.py
+++ covid-vaccine-bot.py
@@ -3,16 +3,17 @@ from twilio.rest import Client
 from bs4 import BeautifulSoup
 import requests
 from slack_webhook import Slack
-import config
+# import config
+import os
 
 # twilio creds
-account_sid = ''
-auth_token = ''
-target_number = '+15555555555'
-source_number = '+15555555555'
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID', '')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN', '')
+target_number = os.environ.get('TWILIO_TARGET_NUMBER', '')
+source_number = os.environ.get('TWILIO_SOURCE_NUMBER', '')
 
 # slack webhook
-slack_url = 'https://hooks.slack.com/services/XXXXXXXXXXX/XXXXXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX'
+slack_url = os.environ.get('SLACK_WEBHOOK_URL', '')
 
 # how many seconds to sleep in between loops
 retry_timer = 15
@@ -28,16 +29,29 @@ vaccine_sites = {'Phoenix Municipal Stadium': 'https://www.handsonphoenix.org/op
                  'State Farm Stadium': 'https://www.handsonphoenix.org/opportunity/a0N1J00000NW4CHUA1'}
 
 # send a message via twilio
+
+
 def send_twilio_message(body):
+    # check if the environment variables are not set first
+    if account_sid == '' or auth_token == '' or target_number == '' or source_number == '':
+        return
     twilioCli = Client(account_sid, auth_token)
-    message = twilioCli.messages.create(body=body, from_=source_number, to=target_number)
+    message = twilioCli.messages.create(
+        body=body, from_=source_number, to=target_number)
 
 # send a message via slack
+
+
 def send_slack_message(slack_url, message):
+    # check if the slack webhook is not set first
+    if slack_url == '':
+        return
     slack = Slack(url=slack_url)
     slack.post(text=message)
 
 # get all content from the site and parse it
+
+
 def get_site(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'}
@@ -45,13 +59,15 @@ def get_site(url):
     soup = BeautifulSoup(response.text, "lxml")
     return soup
 
+
 while True:
     for site, site_url in vaccine_sites.items():
         print('getting site...')
         vaccine_site_info = get_site(site_url)
         print('site retrieved...')
         if str(vaccine_site_info).find("no longer available") == -1:
-            new_message = 'There is an opening at {0}. Check {1} for more info.'.format(site, site_url)
+            new_message = 'There is an opening at {0}. Check {1} for more info.'.format(
+                site, site_url)
             if 'Phoenix Municipal' in site and site_1 == 'on':
                 print(new_message)
                 send_slack_message(slack_url, new_message)
